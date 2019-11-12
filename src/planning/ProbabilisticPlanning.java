cbfce8a0f6fdc041e5265da63371ce0a552af7cf
Abraham Meza Vega <abrahammv_6@hotmail.com>
2019-11-11 18:28:17 -0600

    [New] Tree selection

--- src/planning/ProbabilisticPlanning.java
+++ src/planning/ProbabilisticPlanning.java
@@ -1,15 +1,19 @@
 package planning;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class ProbabilisticPlanning {
 
     private ArrayList<Double> probabilities = new ArrayList<>();
     private ArrayList<Integer> leavesAmount;
+    private Random random = new Random();
+    private double recollectionPercentage;
 
     public ProbabilisticPlanning(ArrayList<Integer> pLeavesAmount, ArrayList<Integer> pDistances,
-                                 int pRecollectionPercentage){
+                                 double pRecollectionPercentage){
         leavesAmount = pLeavesAmount;
+        recollectionPercentage = pRecollectionPercentage;
         calculateProbabilities(pDistances);
     }
 
@@ -25,6 +29,19 @@ public class ProbabilisticPlanning {
             probabilities.set(tree, probability);
             System.out.println(probability);
         }
+    }
+
+    private int selectTree(){
+        for(int tree = 0; tree < probabilities.size(); tree++){
+            if(random.nextDouble() < probabilities.get(tree)){
+                return tree;
+            }
+        }
+        return 0;
+    }
+
+    private int getAntsToSend(int tree){
+        return (int)(leavesAmount.get(tree) * recollectionPercentage);
     }
 
 }
