e9066cc5ce507f1b6f7b4905044b07c7b5284073
Tribeth Rivas <tribethr@gmail.com>
2019-11-15 16:44:55 -0600

    [new] random Three generation

--- src/control/Three.java
+++ src/control/Three.java
@@ -8,8 +8,8 @@ public class Three extends Positionable {
     private int leavesCount;
     private double distance;
 
-    public Three(int pX, int pY, int pDepth) {
-        super(pX, pY);
+    public Three(int pX, int pDepth) {
+        super(pX);
         leavesCount = (int)Math.pow(2,pDepth-1);
         distance = IConstants.ANTHILL_X_POS-x + Calculator.getThreeLenght(pDepth);
     }
